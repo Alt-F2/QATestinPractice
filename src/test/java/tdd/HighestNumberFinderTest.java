ad3f1f31645b18c6b10d2df9dd494b75a4b53b24
Andrew I <AndrewI@lbg.com>
2024-10-31 09:53:32 +0000

    third test completed x items in array where largest value is x element

--- src/test/java/tdd/HighestNumberFinderTest.java
+++ src/test/java/tdd/HighestNumberFinderTest.java
@@ -8,7 +8,7 @@ public class HighestNumberFinderTest {
     @Test
     public void find_highest_in_array_of_one_expect_single_item(){
         // arrange
-        int input[] = {10};
+        int[] input = {10};
         HighestNumberFinder cut = new HighestNumberFinder();
         int expectedResult = 10;
 
@@ -22,7 +22,7 @@ public class HighestNumberFinderTest {
     @Test
     public void find_highest_number_in_an_array_of_two_descending_expect_first_element(){
         // arrange
-        int input[] = {13, 7};
+        int[] input = {13, 7};
         HighestNumberFinder cut = new HighestNumberFinder();
         int expectedResult = 13;
 
@@ -36,9 +36,23 @@ public class HighestNumberFinderTest {
     @Test
     public void find_highest_number_in_an_array_of_two_ascending_expect_second_element(){
         // arrange
-        int input[] = {7, 13};
+        int[] input = {7, 13};
         HighestNumberFinder cut = new HighestNumberFinder();
         int expectedResult = 13;
+
+        // act
+        int actualResult = cut.findHighestNumber(input);
+
+        // assert
+        assertEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    public void find_highest_number_in_an_array_of_x_ascending_expect_x_element(){
+        // arrange
+        int[] input = {4, 5, -8, 3, 11, -21, 6};
+        HighestNumberFinder cut = new HighestNumberFinder();
+        int expectedResult = 11;
 
         // act
         int actualResult = cut.findHighestNumber(input);
